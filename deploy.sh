d425c3eecfa5382685e8d858252c859cc8644a1a
abr4hamw <abraham4woods@gmail.com>
2026-04-27 12:29:01 -0600

    Actualizacion

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+echo 'Bajamos repositorio actualizado'
 #Copiamos código actualizado de Github
 git pull origin main
 
+echo 'Detenemos Cotenedores de la Aplicacion'
 #Detenemos contenedores
 docker-compose down
 
+echo 'Inicializamos contenedores y reconstruimos imagen actualizada'
 #Inicializamos contenedores y reconstruimos
 docker-compose -f docker-compose.production.yml up -d --build
+
+echo 'Actualizacion finalizada'
