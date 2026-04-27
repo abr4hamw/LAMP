040b0a72daa3da0b29f29be16624db2d4a5df128
abr4hamw <abraham4woods@gmail.com>
2026-04-27 12:39:59 -0600

    Bold

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if($conn->connect_error){
     </head>
 
     <body>
-        <h1>Stack LAMP en Docker ITL</h1>
+        <h1>Stack LAMP en Docker <strong>ITL</strong></h1>
         <p class="ok">Conexión a MySQL exitosa</p>
 
         <div class="info">
